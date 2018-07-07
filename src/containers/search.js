ea328a675a54a882d0f1efa5f2bef98ec1e75920
Jess Sze <spkjess@gmail.com>
2018-07-07 17:04:23 +0800

    remove example code

--- src/containers/search.js
+++ src/containers/search.js
@@ -2,26 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
-import PersonIcon from '@material-ui/icons/Person';
-import AddIcon from '@material-ui/icons/Add';
-import Typography from '@material-ui/core/Typography';
-import blue from '@material-ui/core/colors/blue';
 import './search.css';
 
 const items = ['Packaged Food', 'Fresh Food', 'Cooked Food'];
-const styles = {
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-};
+const styles = {};
 
 class SimpleDialog extends React.Component {
   handleClose = () => {
@@ -37,26 +25,17 @@ class SimpleDialog extends React.Component {
 
     return (
       <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" {...other}>
-        <DialogTitle id="simple-dialog-title">CATEGORY</DialogTitle>
+        <DialogTitle id="simple-dialog-title">Category</DialogTitle>
         <div>
           <List>
             {items.map(item => (
-              <ListItem button onClick={() => this.handleListItemClick(item)} key={item}>
-                <ListItemAvatar>
-                  <Avatar className={classes.avatar}>
-                    <PersonIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={item} />
+              <ListItem>
+                <Button variant="outlined" color="primary" onClick={() => this.handleListItemClick(item)}>{item}</Button>
               </ListItem>
+              
             ))}
             <ListItem button onClick={() => this.handleListItemClick('addAccount')}>
-              <ListItemAvatar>
-                <Avatar>
-                  <AddIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="add account" />
+              <Button variant="contained" color="primary" onClick={this.handleClickOpen}>Search</Button>
             </ListItem>
           </List>
         </div>
