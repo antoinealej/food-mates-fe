b8948f8aad3c3b39b9c662f224828d6ee5acc769
viviane.wu.mac <viviane.wu.mac@axa.com.hk>
2018-07-08 00:08:22 +0800

    added search route

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import HomePage from './containers/homepage';
 import FoodForm from './containers/foodForm';
+import Search from './containers/search';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path='form' component={FoodForm}/>
+    <Route path='search' component={Search} />
   </Route>
 );
