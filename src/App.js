19a1318cc3ee02bc908c8c309cd3415792bb8e51
yikais <yikai@blueplume.org>
2018-07-07 14:03:57 +0800

    added home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <Button variant="contained" color="primary">
-          Hello World
+      <div className="homepage">
+        <Button variant="contained" color="primary" href="/containers/login.js">
+          Login page
         </Button>
       </div>
     );
